d0808bff412ac8691df174b4d3768028e15b93f2
JP Pham <JPham97@tamu.edu>
2022-11-03 22:02:05 -0500

    added react stuff

---
+++ pom-honey-react/src/App.js
@@ -0,0 +1,15 @@
+import pomHoneyLogo from './assets/logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <img src={pomHoneyLogo} className="App-logo" alt="logo"/>
+        <p>Howdy</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
