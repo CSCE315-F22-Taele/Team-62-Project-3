4699c456bdcc7249faac0336550ecd8418e6ae4d
JP Pham <JPham97@tamu.edu>
2022-11-04 15:32:54 -0500

    added server stuff to react front end

--- pom-honey-react/src/App.js
+++ pom-honey-react/src/App.js
@@ -1,12 +1,25 @@
 import pomHoneyLogo from './assets/logo.png';
 import './App.css';
+import { useState } from 'react';
+
+
+const callAPI = (setApiResponse) => {
+  fetch("http://localhost:8080/manager/orders")
+    .then(res => res.text())
+    .then(res => setApiResponse(res));
+    
+}
 
 function App() {
+  const [apiResponse, setApiResponse] = useState("");
+  callAPI(setApiResponse);
+
   return (
     <div>
       <header>
         <img src={pomHoneyLogo} className="App-logo" alt="logo"/>
         <p>Howdy</p>
+        <p><b>Server side stuff:</b> {apiResponse}</p>
       </header>
     </div>
   );
